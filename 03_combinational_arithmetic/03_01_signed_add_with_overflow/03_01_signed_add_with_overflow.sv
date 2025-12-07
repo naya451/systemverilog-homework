48de816924de6e2b02cb906d3026e96a93c4588f
Anastasia Belova <abelova@astralinux.ru>
2025-12-07 16:11:46 +0300

    add two tasks of 3rd homework

--- 03_combinational_arithmetic/03_01_signed_add_with_overflow/03_01_signed_add_with_overflow.sv
+++ 03_combinational_arithmetic/03_01_signed_add_with_overflow/03_01_signed_add_with_overflow.sv
@@ -37,6 +37,11 @@ module signed_add_with_overflow
   // 4-bit maximum or minimum signed number.
   //
   // Otherwise the 'overflow' should be set to 0.
-
+  logic carry[3:0];
+  assign {carry[0], sum[0]} = {(a[0] & b[0]), a[0] ^ b[0]};
+  assign {carry[1], sum[1]} = {(a[1] & b[1]) | (carry[0] & (a[1] ^ b[1])), a[1] ^ b[1] ^ carry[0]};
+  assign {carry[2], sum[2]} = {(a[2] & b[2]) | (carry[1] & (a[2] ^ b[2])), a[2] ^ b[2] ^ carry[1]};
+  assign {carry[3], sum[3]} = {(a[3] & b[3]) | (carry[2] & (a[3] ^ b[3])), a[3] ^ b[3] ^ carry[2]};
+  assign overflow = (carry[3] ^ carry[2]);
 
 endmodule
--- 03_combinational_arithmetic/03_02_signed_add_with_saturation/03_02_signed_add_with_saturation.sv
+++ 03_combinational_arithmetic/03_02_signed_add_with_saturation/03_02_signed_add_with_saturation.sv
@@ -35,6 +35,13 @@ module signed_add_with_saturation
   // When the result does not fit into 4 bits,
   // and the arguments are negative,
   // the sum should be set to the minimum negative number.
-
+  logic overflow;
+  logic [3:0] tmp, carry;
+  assign {carry[0], tmp[0]} = {(a[0] & b[0]), a[0] ^ b[0]};
+  assign {carry[1], tmp[1]} = {(a[1] & b[1]) | (carry[0] & (a[1] ^ b[1])), a[1] ^ b[1] ^ carry[0]};
+  assign {carry[2], tmp[2]} = {(a[2] & b[2]) | (carry[1] & (a[2] ^ b[2])), a[2] ^ b[2] ^ carry[1]};
+  assign {carry[3], tmp[3]} = {(a[3] & b[3]) | (carry[2] & (a[3] ^ b[3])), a[3] ^ b[3] ^ carry[2]};
+  assign overflow = (carry[3] ^ carry[2]);
+  assign sum = overflow ? (a[3] ? 'b1000 : 'b0111) : tmp;
 
 endmodule
